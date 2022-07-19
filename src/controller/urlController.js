78a185d5d3e684144bd46039efa960d1556d9564
gautam mehta <mehtag810@gmail.com>
2022-07-19 11:50:17 +0530

    phase 1 done

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -14,7 +14,7 @@ const createShortUrl = async function (req, res) {
             if (Object.keys(data).length == 0) {
                   return res.status(400).send({
                         status: false,
-                        message: " please enter the data in body "
+                        message: " Please Provide long Url "
                   })
             }
 
@@ -22,7 +22,7 @@ const createShortUrl = async function (req, res) {
             if (!regLongUrl.test(longUrl)) {
                   return res.status(400).send({
                         status: false,
-                        message: " please provide valid URL "
+                        message: " Please Provide a Valid long URL "
                   })
             }
 
@@ -37,25 +37,26 @@ const createShortUrl = async function (req, res) {
             const baseUrl = "http://localhost:3000";
             const shortUrl = baseUrl + "/" + shortCode;
 
-            const checkLongUrl = await urlModel.findOne({ longUrl: longUrl })
+            const checkLongUrl = await urlModel.findOne({ longUrl: longUrl }).select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 1 })
             if (checkLongUrl) {
                   return res.status(400).send({
                         status: false,
                         message: `longUrl is already present in the database`,
                         data: checkLongUrl
-                        // data: `http://localhost:3000/${checkLongUrl.urlCode}`
                   })
             }
 
-            const ShortenUrl = await urlModel.create({ longUrl: longUrl, shortUrl: shortUrl, urlCode: shortCode, });
+            await urlModel.create({ longUrl: longUrl, shortUrl: shortUrl, urlCode: shortCode })
+            const saveData = await urlModel.findOne({longUrl:longUrl}).select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 1 })
+            
+            
             return res.status(201).send({
                   status: true,
-                  message: " successfully created ",
-                  data: ShortenUrl
+                  message: " Successfully Created Shorten Url ",
+                  data: saveData
             })
       }
       catch (err) {
-            console.log(err.message)
             return res.status(500).send({
                   status: false,
                   message: err.message
@@ -64,12 +65,11 @@ const createShortUrl = async function (req, res) {
 }
 
 
-//******************************************* getUrl **********************************/
+// ******************************************* getUrl **********************************/
 
 const getUrl = async function (req, res) {
       try {
             const urlCode = req.params.urlCode
-            console.log(urlCode)
 
             if (!(shortId.isValid(urlCode))) {
                   return res.status(404).send({
@@ -80,17 +80,14 @@ const getUrl = async function (req, res) {
 
             let checkUrl = await urlModel.findOne({ urlCode: urlCode })
             if (!checkUrl) {
-                  return res.status(404).send({
+                  return res.status(400).send({
                         status: false,
                         message: "data not found"
                   })
             }
 
-            return res.status(302).send({
-                  status: true,
-                  message: "successfull responce",
-                  data: checkUrl.longUrl
-            });
+            return res.status(302).redirect({
+                  data:checkUrl.longUrl})
 
       } catch (err) {
             return res.status(500).send({
