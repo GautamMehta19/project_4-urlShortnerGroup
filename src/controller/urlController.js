562cdb904a1875875a41ec796a2908080d348eac
gautam mehta <mehtag810@gmail.com>
2022-07-20 17:34:32 +0530

    phase2

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,6 +1,32 @@
 const urlModel = require("../model/urlModel")
 const shortId = require("shortid")
 const mongoose = require("mongoose")
+const redis = require("redis");
+const { promisify } = require("util");
+
+
+//Connect to redis
+const redisClient = redis.createClient(
+      10354,
+      "redis-10354.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+
+      { no_ready_check: true }
+);
+redisClient.auth("vsfE1zHabXclFvHEvbntt3Ho5Ohn6VEd", function (err) {
+      if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+      console.log("Connected to Redis..");
+});
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 
 //******************************************* createShortUrl **********************************/
@@ -47,9 +73,10 @@ const createShortUrl = async function (req, res) {
             }
 
             await urlModel.create({ longUrl: longUrl, shortUrl: shortUrl, urlCode: shortCode })
-            const saveData = await urlModel.findOne({longUrl:longUrl}).select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 1 })
-            
-            
+            const saveData = await urlModel.findOne({ longUrl: longUrl }).select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 1 })
+
+            await SET_ASYNC(`${req.params.authorId}`, JSON.stringify(saveData))
+
             return res.status(201).send({
                   status: true,
                   message: " Successfully Created Shorten Url ",
@@ -86,8 +113,18 @@ const getUrl = async function (req, res) {
                   })
             }
 
-            return res.status(302).redirect({
-                  data:checkUrl.longUrl})
+
+            let cahcedProfileData = await GET_ASYNC(`${urlCode}`)
+            if (cahcedProfileData) {
+                  console.log(cahcedProfileData)
+                  res.send(cahcedProfileData)
+            } else {
+                  let profile = await urlModel.findById(checkUrl);
+                  await SET_ASYNC(`${checkUrl}`, JSON.stringify(profile))
+                  return res.status(302).redirect(checkUrl.longUrl)
+            }
+
+            // return res.status(302).redirect(checkUrl.longUrl)
 
       } catch (err) {
             return res.status(500).send({
@@ -100,3 +137,34 @@ const getUrl = async function (req, res) {
 
 module.exports.createShortUrl = createShortUrl
 module.exports.getUrl = getUrl
+
+
+
+
+
+
+
+
+
+
+
+// const createAuthor = async function (req, res) {
+//       let data = req.body;
+//       let authorCreated = await authorModel.create(data);
+//       res.send({ data: authorCreated });
+// };
+
+// const fetchAuthorProfile = async function (req, res) {
+//       let cahcedProfileData = await GET_ASYNC(`${req.params.authorId}`)
+//       if (cahcedProfileData) {
+//             res.send(cahcedProfileData)
+//       } else {
+//             let profile = await authorModel.findById(req.params.authorId);
+//             await SET_ASYNC(`${req.params.authorId}`, JSON.stringify(profile))
+//             res.send({ data: profile });
+//       }
+
+// };
+// ``
+// module.exports.createAuthor = createAuthor;
+// module.exports.fetchAuthorProfile = fetchAuthorProfile;
